db6b1230ba9741972cb08a11910e968c62f33115
Roderic Bos <github@rooc.nl>
2023-10-14 19:17:29 +0200

    pivot1

--- main.c
+++ main.c
@@ -1,117 +1,54 @@
 #include <stdio.h>
 #include "libs/raylib.h"
 
-#define NROFCELLS  20
+#define SPEED 10
+#define SHAPESIZE 5
+#define NROFCELLSX 40
+#define LINECOLOR CLITERAL(Color){ 0, 0, 255, 80}
 
 static float dt;
 int screenWidth = 800;
 int screenHeight = 600;
-int gridOffset = 20;
 
 int cellWidth = 0; //calculated every tick
 
 typedef struct {
-    int fields[NROFCELLS][NROFCELLS];
-} Board;
-
-typedef struct {
-    int items[4][4];
+    int items[SHAPESIZE][SHAPESIZE];
+    Vector2 gridPos;
+    Vector2 pos;
 } Shape;
 
-
-Vector2 mpos = {0};
-Board board = {0};
 Shape s1 = {
     .items = {
-        {0,0,0,0},
-        {0,0,0,2},
-        {2,2,1,2},
-        {0,0,0,0}
-    }
+        {0,0,0,0,0},
+        {0,0,3,0,0},
+        {2,2,1,2,2},
+        {2,2,2,2,2},
+        {2,0,2,0,2}
+    },
+    .gridPos = { .x = 10, .y = 25 },
+    .pos = {0}
 };
 
-Shape RotateShape(Shape s) {
-    int n = 4;
-    // Transpose the s.items
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            int temp = s.items[i][j];
-            s.items[i][j] = s.items[j][i];
-            s.items[j][i] = temp;
-        }
-    }
-    // Reverse the columns
-    for (int i = 0; i < n; i++) {
-        for (int j = 0, k = n - 1; j < k; j++, k--) {
-            int temp = s.items[j][i];
-            s.items[j][i] = s.items[k][i];
-            s.items[k][i] = temp;
-        }
-    }
-    return s;
-}
+void DrawPlayer() {
+    //update gridpos from pos
+    s1.gridPos.x = (int)(s1.pos.x / cellWidth);
 
-void DrawShape(Shape s, int gridX, int gridY, bool picked) {
-    int offsetX = 0;
-    int offsetY = 0;
-    for(int x = 0; x < 4; x++) {
-        for(int y = 0; y < 4; y++) {
-            if (s.items[y][x] == 1) {
-                offsetX = x;
-                offsetY = y;
-                break;
+    for (int x = 0; x < SHAPESIZE; x++) {
+        for (int y = 0; y < SHAPESIZE; y++) {
+            Color c = BLACK;
+            switch (s1.items[y][x])
+            {
+                case 1: c = RED; break;
+                case 2: c = BLUE; break;
+                case 3: c = GREEN; break;
             }
+            DrawRectangle(s1.pos.x+ x*cellWidth, s1.gridPos.y*cellWidth + y*cellWidth, cellWidth, cellWidth, c);
+
         }
-        if (offsetX || offsetY) break;
-    }
-
-	Color cellColor = CLITERAL(Color){255,200,100,255};
-    if (picked) {
-        TraceLog(LOG_INFO, "picked");
-    }
-    bool allowed = true;
-    //check if its allowed
-    for(int x = 0; x < 4; x++) {
-        for(int y = 0; y < 4; y++) {
-            if (s.items[y][x]) {
-                int cellGridX = gridX + x - offsetX;
-                int cellGridY = gridY + y - offsetY;
-
-                if (board.fields[cellGridX][cellGridY]){
-                    allowed = false;
-                    break;
-                }
-                //todo: empty rows/columns in shape
-                if (gridX - offsetX < 0) {
-                    allowed = false;
-                    break;
-                }
-                if (gridY - offsetY < 0) {
-                    allowed = false;
-                    break;
-                }
-            }
-        }
-    }
-
-
-    for(int x = 0; x < 4; x++) {
-        for(int y = 0; y < 4; y++) {
-            if (s.items[y][x]) {
-                int cellGridX = gridX + x - offsetX;
-                int cellGridY = gridY + y - offsetY;
-                if (allowed && picked && (cellGridX < NROFCELLS) && (cellGridX < NROFCELLS)) {
-                    board.fields[cellGridX][cellGridY] = 1;
-                }
-
-                int xpos = cellGridX*cellWidth;
-                int ypos = cellGridY*cellWidth;
-                DrawRectangle(xpos + gridOffset, ypos + gridOffset, cellWidth, cellWidth, allowed ? cellColor : PINK);
-            }
-        }
-
     }
 }
+double gametime = 0;
 
 int main(void) {
     Color gridColor = CLITERAL(Color){ 180, 41, 55, 255 };
@@ -122,47 +59,33 @@ int main(void) {
 
     while(!WindowShouldClose()) {
         dt = GetFrameTime();
+        gametime += dt;
         screenWidth = GetRenderWidth();
         screenHeight = GetRenderHeight();
-        cellWidth = screenWidth / 40;
-        mpos = GetMousePosition();
+        cellWidth = screenWidth / NROFCELLSX;
+        s1.gridPos.y = (screenHeight / cellWidth) - SHAPESIZE;
 
-        if (IsKeyReleased(KEY_R)) {
-            s1 = RotateShape(s1);
+        if (IsKeyDown(KEY_A)) {
+            s1.pos.x -= SPEED;
+            if (s1.pos.x < 0) s1.pos.x = 0;
+        }
+        if (IsKeyDown(KEY_D)) {
+            s1.pos.x += SPEED;
+            if (s1.pos.x  > screenWidth - SHAPESIZE*cellWidth) s1.pos.x = screenWidth - SHAPESIZE*cellWidth;
         }
 
         BeginDrawing();
-        ClearBackground(DARKGRAY);
-        DrawText("Game1", 200, 10, 10, WHITE);
-       for(int x = 0; x < NROFCELLS; x++) {
-            for(int y = 0; y < NROFCELLS; y ++) {
-                int xpos = x*cellWidth + gridOffset;
-                int ypos = y*cellWidth + gridOffset;
+        ClearBackground(BLACK);
 
-                switch (board.fields[x][y]) {
-                    case 1:
-        	            DrawRectangle(xpos, ypos, cellWidth, cellWidth, gridColor);
-                        break;
+        DrawPlayer();
 
-                    default:
-        	            DrawRectangleLines(xpos, ypos, cellWidth, cellWidth, gridColor);
-                        break;
-                }
-
-
-            }
+        for (int i = 0; i < screenWidth+cellWidth/cellWidth; i++) {
+            DrawLine(i*cellWidth, 0, i*cellWidth, screenHeight, LINECOLOR);
         }
-         for(int x = 0; x < NROFCELLS; x++) {
-            for(int y = 0; y < NROFCELLS; y ++) {
-                int xpos = x*cellWidth + gridOffset;
-                int ypos = y*cellWidth + gridOffset;
-
-				if (CheckCollisionPointRec(mpos, CLITERAL(Rectangle){xpos, ypos, cellWidth-1, cellWidth})) {
-                    DrawShape(s1, x, y, IsMouseButtonReleased(MOUSE_LEFT_BUTTON));
-                    break;
-				}
-            }
+        for (int i = 0; i < screenHeight+cellWidth/cellWidth; i++) {
+            DrawLine(0, i*cellWidth, screenWidth, i*cellWidth, LINECOLOR);
         }
+
         EndDrawing();
     }
 
