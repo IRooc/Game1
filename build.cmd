e3066e7bbf7a091cbf3688fc45b05a9c58c01699
Roderic Bos <github@rooc.nl>
2024-02-04 11:01:21 +0100

    basic wasm setup

--- build.cmd
+++ build.cmd
@@ -1,1 +1,5 @@
-cl /Zi /I libs\  main.c /link libs/raylib.lib opengl32.lib user32.lib gdi32.lib winmm.lib kernel32.lib msvcrt.lib shell32.lib
+REM cl /Zi /I libs\  main.c /link libs/raylib.lib opengl32.lib user32.lib gdi32.lib winmm.lib kernel32.lib msvcrt.lib shell32.lib
+clang -o main.exe main.c --no-standard-libraries -L./libs -lraylib -lopengl32 -luser32 -lmsvcrt -lgdi32 -lshell32 -lwinmm
+
+REM webassembly
+clang --target=wasm32 -o main.wasm main.c --no-standard-libraries -Wl,--no-entry -Wl,--export=main -Wl,--allow-undefined
